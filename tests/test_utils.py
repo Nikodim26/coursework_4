8e8a9bb07bc0ea58bbb8ce0fb6160edb80b9934e
Андрей Васильев <lr1975@yandex.ru>
2026-08-26 15:24:46 +0400

    осталось удаление

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,12 @@
 import os
-from utils import translate_text, obtaining_information_on_the_criteria
 
 from src.receipt_by_criterion import ReceiptByCriterion
+from src.utils import obtaining_information_on_the_criteria
+from src.utils import translate_text
 
 
 def test_obtaining_information_on_the_criteria(fixture_get_by_criterion) -> None:
-
+    receipt_by_criterion = ReceiptByCriterion("1.json", [])
     assert obtaining_information_on_the_criteria("1.json",fixture_get_by_criterion) == [
         {"Altitude": 12146.28, "Country": "France", "ICAO24": "39de41", "Velocity": 236.75},
         {"Altitude": 388.62, "Country": "Germany", "ICAO24": "3d23da", "Velocity": 72.7},
